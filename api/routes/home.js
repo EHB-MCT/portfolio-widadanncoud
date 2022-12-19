9f7bdf382aa8c74250b1c9083693e92d68efb340
widad anncoud <widad.anncoud@student.ehb.be>
2022-12-19 16:58:32 +0100

    feat: test sql query

--- api/routes/home.js
+++ api/routes/home.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../database/connection.js')
 /* GET home page. */
 router.get("/", async (req, res) => {
-  res.send('Hello Widad !!!')
+  const selectUserQuery = 'SELECT * FROM users';
+
+  connection.query(selectUserQuery, (err, results, fields) => {
+    if (err) throw err;
+
+    console.log('results:', results)
+    res.send('Hello Widad !!!')
+  });
 })
 
 module.exports = router;
