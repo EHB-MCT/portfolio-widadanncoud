1af1c88fa946659e5418aef01a05ccca6fa769c3
widadanncoud <widad.anncoud@student.ehb.be>
2022-12-21 00:25:53 +0100

    feat: adding views and html,css to index page

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const http = require('http');
@@ -7,9 +8,10 @@ const port = process.env.PORT || '3000';
 app.set('port', port);
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../views')));
 
 // IMPORT ROUTES
-const homeRouter = require('../routes/home');
+const homeRouter = require('../routes');
 
 // API ROUTES
 app.use('/', homeRouter);
