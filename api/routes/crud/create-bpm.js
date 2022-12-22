cd457678f23a23f7f50d0ae2905acb0dc403ceae
widadanncoud <widad.anncoud@student.ehb.be>
2022-12-22 18:30:03 +0100

    feat: adding sql query

--- api/routes/crud/create-bpm.js
+++ api/routes/crud/create-bpm.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const connection = require('../../database/connection');
 
 /* GET home page. */
 router.post("/", async (req, res) => {
-    console.log(req.body)
-    res.send("BPM is created")
+    const {quantity, date, time, message} = req.body
+
+    connection.query('INSERT INTO bpm (`quantity`, `date`, `time`, `message`) VALUES (?,?,?,?)', [quantity, date, time, message],
+    (error, results) => {
+     if (error) return res.json({ error: error });
+        console.log('results:', results)
+     });
+    
+    res.send(`A BPM of ${quantity} is created`);
 })
   
 module.exports = router;
--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const http = require('http');
@@ -9,6 +10,10 @@ app.set('port', port);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../views')));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
 // IMPORT ROUTES
 const indexRouter = require('../routes/index');
@@ -18,7 +23,7 @@ const createBPM = require('../routes/crud/create-bpm');
 // API ROUTES
 app.use('/', indexRouter);
 app.use('/bpm', bpmRouter);
-app.use('/creat-bpm', createBPM);
+app.use('/crud/create-bpm', createBPM);
 
 // CREATE SERVER
 var server = http.createServer(app);
